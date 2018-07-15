5351a44976746cbcae5ab6480724bed85882d9f8
SmuurfBR <39779447+SmuurfBR@users.noreply.github.com>
2018-07-15 20:43:39 -0300

    asdasd

---
+++ comandos/fortnite.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const Fortnite = require("fortnite");
+const ft = new Fortnite(process.env.FORTNITE_API);
+
+exports.run = (client, message, args, ops, tools) => {
+  message.delete();
+  //!fortnite SmuurfBR PC
+  let username = args[0];
+  let plataform = args[1] || "pc";
+
+  let data = ft.getInfo(username, plataform).then(data => {
+
+      let stats = data.lifetimeStats;
+      let kills = stats.find(s => s.stat == 'kills');
+      let wins = stats.find(s => s.stat == 'wins');
+      let kd = stats.find(s => s.stat == 'kd');
+      let mPlayed = stats.find(s => s.stat == 'matchesPlayed');
+      let tPlayed = stats.find(s => s.stat == 'timePlayed');
+      let asTime = stats.find(s => s.stat == 'avgSurvivalTime');
+
+      let embed = new Discord.RichEmbed()
+      .setTitle("Fortnite Stats")
+      .setAuthor(data.username)
+      .setColor(0xffffff)
+      .addField("🔪 | `Kills`", kills.value, true)
+      .addField("🏆 | `Vitorias`", wins.value, true)
+      .addField("🛡 | `K/D`", kd.value, true)
+      .addField("🔎 | `Partidas`", mPlayed.value, true)
+      .addField("📊 | `Tempo jogado`", kills.value, true)
+      .addField("📈 | `Tempo de sobrevivência`", kills.value, true)
+
+    message.channel.send(embed);
+
+  }).catch(e => {
+      console.log(e);
+      message.channel.send(":exclamation: **| Não foi possivel encontrar o usúario**")
+  });
+
+}
